2562f8c4dac04b97982705b5419c823e23566983
Matt <mattymeans@gmail.com>
2025-08-07 21:17:44 -0600

    Change to text siez

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home(){
     return(
-      <div className='flex flex-col gap-15 text-center text-3xl md:text-5xl border-15 border-double md:m-10 md:p-15 rounded-4xl grow bg-white'>
+      <div className='flex flex-col gap-25 text-center text-3xl md:text-5xl lg:text-7xl border-15 border-double md:m-10 md:p-15 rounded-4xl grow bg-white'>
         <h1>Welcome to the best site for finding All Keyboard Shortcuts!</h1>
         <p>The Mission: Create a singular website where anyone can find the right keyboard shortcuts for any application, on any system, to increase productivity!</p>
         <p>Every list was manually compiled using the official documents from each of the companies that produced them.</p>
